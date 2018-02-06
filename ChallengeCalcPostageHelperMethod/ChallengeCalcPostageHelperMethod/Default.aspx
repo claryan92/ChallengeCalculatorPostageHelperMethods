e00ee5f1e0d5539bf7c86c5e54186b37b6654548
Ryan <claryan92@gmail.com>
2018-02-06 09:45:24 -0800

    Postage Calculator methods

---
+++ ChallengeCalcPostageHelperMethod/ChallengeCalcPostageHelperMethod/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeCalcPostageHelperMethod.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	Postage Calculator<br />
+			<br />
+			Width:&nbsp;
+			<asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="handleChange"></asp:TextBox>
+			<br />
+			Height:&nbsp;
+			<asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="handleChange"></asp:TextBox>
+			<br />
+			Length:&nbsp;
+			<asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="handleChange"></asp:TextBox>
+			<br />
+			<br />
+			<asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="methodGroup" OnCheckedChanged="handleChange" Text="Ground" />
+			<br />
+			<asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="methodGroup" OnCheckedChanged="handleChange" Text="Air" />
+			<br />
+			<asp:RadioButton ID="nextdayRadioButton" runat="server" AutoPostBack="True" GroupName="methodGroup" OnCheckedChanged="handleChange" Text="Next Day" />
+			<br />
+			<br />
+			<asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeCalcPostageHelperMethod/ChallengeCalcPostageHelperMethod/Default.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeCalcPostageHelperMethod
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void handleChange(object sender, EventArgs e)
+		{
+			performCalculation();
+		}
+		private void performCalculation()
+		{
+			// Are there values in the text and checkboxes?
+			if (!valuesExist()) return;
+
+
+			//What is the Volume?
+			int volume = 0;
+			if (!tryGetVolume(out volume)) return;
+
+			//Which multiplier is used?
+			double postageMultiplier = getPostageMultiplier();
+
+			//Determine the cost
+			double cost = volume * postageMultiplier;
+
+			//Show the result
+			resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", cost);
+		}
+
+		private bool valuesExist()
+		{
+			if (!airRadioButton.Checked
+				&& !groundRadioButton.Checked
+				&& !nextdayRadioButton.Checked)
+				return false;
+
+			if (widthTextBox.Text.Trim().Length == 0 
+				|| heightTextBox.Text.Trim().Length == 0)
+				return false;
+
+			return true;
+		}
+
+		private bool tryGetVolume(out int volume)
+		{
+			volume = 0;
+			int width = 0;
+			int height = 0;
+			int length = 0;
+
+			if (!int.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+			if (!int.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+			if (!int.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1;
+
+			volume = width * height * length;
+			return true;
+
+		}
+
+		private double getPostageMultiplier()
+		{
+			if (groundRadioButton.Checked) return .15;
+			else if (airRadioButton.Checked) return .25;
+			else if (nextdayRadioButton.Checked) return .45;
+			else return 0;
+		}
+
+	}
+}
